b44f3243e736c83f49b5dc7109fcefbb13555cf9
vgodwinamz <87547275+vgodwinamz@users.noreply.github.com>
2021-07-28 17:12:22 -0600

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World. Welcome to Godwins world"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
